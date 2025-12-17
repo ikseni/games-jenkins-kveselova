8da9c27ef6d97e3c355c4d2638709b7a2be15fa9
Ksenia Veselova <veselovaksenia@gmail.com>
2025-12-17 20:33:25 +0300

    Добавила комментарии в Calculator class

--- src/main/java/org/douggschwind/games/Calculator.java
+++ src/main/java/org/douggschwind/games/Calculator.java
@@ -5,18 +5,43 @@ package org.douggschwind.games;
  */
 public class Calculator {
     
+    /**
+     * Складывает два числа.
+     * @param a первое число
+     * @param b второе число  
+     * @return сумма a и b
+     */
     public int add(int a, int b) {
         return a + b;
     }
     
+    /**
+     * Вычитает b из a.
+     * @param a уменьшаемое
+     * @param b вычитаемое
+     * @return разность a и b
+     */
     public int subtract(int a, int b) {
         return a - b;
     }
     
+    /**
+     * Умножает два числа.
+     * @param a первый множитель
+     * @param b второй множитель
+     * @return произведение a и b
+     */
     public int multiply(int a, int b) {
         return a * b;
     }
     
+    /**
+     * Делит a на b.
+     * @param a делимое
+     * @param b делитель
+     * @return результат деления a на b
+     * @throws IllegalArgumentException если b равно 0
+     */
     public double divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
@@ -24,6 +49,11 @@ public class Calculator {
         return (double) a / b;
     }
     
+    /**
+     * Проверяет, является ли число положительным.
+     * @param number проверяемое число
+     * @return true если число положительное, иначе false
+     */
     public boolean isPositive(int number) {
         return number > 0;
     }
